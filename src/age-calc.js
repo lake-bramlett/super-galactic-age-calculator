0a638f4fd4e9e957be9202449c1fa9f1d50d2fe7
Lake Bramlett <lake.bramlett@gmail.com>
2019-06-28 10:10:51 -0700

    reconfigure package.json for windows-friendly scripts

---
+++ src/age-calc.js
@@ -0,0 +1,14 @@
+export class Person {
+  constructor(dob, gender, country) {
+    this.dob = dob;
+    this.gender = gender;
+    this.country = country;
+    this.age = 0
+  };
+
+    getAge() {
+      const today = new Date();
+      const userDOB = new Date(this.dob);
+      this.age.earth = today.getYear() - userDOB.getYear();
+    };
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
+import { User } from './age-calc.js';
 import $ from 'jquery';
 
 $(document).ready(function () {
-
+  console.log('jquery enabled');
+  $('button').click(function(event) {
+    event.preventDefault();
+    const dateOfBirth = $('.dob').val();
+    const gender = $('.gender-select').val();
+    const country = $('.country-select').val();
+    let user = new Person (dateOfBirth, gender, country);
+  });
 
 });
