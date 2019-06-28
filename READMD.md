462e5724c090107b6a185712bf1d27b61a3c32ef
Lake Bramlett <lake.bramlett@gmail.com>
2019-06-28 14:38:29 -0700

    add ninth spec detailing front end condition if user enters future date

--- READMD.md
+++ READMD.md
@@ -17,6 +17,7 @@
   | Return user's remaining years left to live in Earth years | DoB: June 4th, 1989; Gender: Male; Country: USA; Planet: Earth | Years remaining: 46 years |
   | Return user's remaining years left to live on any planet and from any country | DoB: June 4th, 1989; Gender: Female; Country: France; Planet: Jupiter | Years remaining: 83 years |
   | Return user's years surpassed if age is greater than life expectancy | DoB: June 4th, 1989; Gender: Male; Country: USA; Planet: Mercury | Years over life expectancy: 49 |
+  | Returns error message if user enters a future date and/or hasn't been born yet | DoB: June 4th, 2989 | "Nice try future person! You haven't been born yet! Try again in 970 years!" |
 
 3. Integration
   * Write spec tests
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { Person } from './age-calc.js';
 import $ from 'jquery';
+import { Person } from './age-calc.js';
 import './styles.css';
 
 
@@ -13,14 +13,21 @@ $(document).ready(function () {
     let user = new Person (dateOfBirth, planet, gender, country);
     user.getAge();
     user.getLifeExpectancy();
-    $('.output').show();
-    $('.output span.planet').text(user.planet);
-    $('.output span.planet-age').text(user.age);
-    $('.output span.life-expectancy').text(user.expectancy);
-    if (user.expectancyOverUnder() === true) {
-      $('.output span.years-left').text(`${user.yearsRemaining} years left to live on `)
-    } else {
+    if (user.age >= 0) {
+      $('.default').hide();
+      $('.output').show();
+      $('.output span.planet').text(user.planet);
+      $('.output span.planet-age').text(user.age);
+      $('.output span.life-expectancy').text(user.expectancy);
+      if (user.expectancyOverUnder() === true) {
+        $('.output span.years-left').text(`${user.yearsRemaining} years left to live on `)
+      } else {
         $('.output span.years-left').text(`surpassed the life expectancy by ${user.yearsOver} years on `)
+      }
+    } else {
+      $('.output').hide();
+      $('.default').show();
+      $('.default span.try-again').text(user.age * -1);
     }
   });
 
